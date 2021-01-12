048bc57df2798d92db313398ca662ee72bd4e9bf
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2021-01-11 22:10:51 -0300

    last commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import * as CourseActions from '../../store/actions/course';
 
 // não esquecer das {} ao passar os "modules" pois é um objeto
-const Sidebar = ({ modules, dispatch }) => (
+const Sidebar = ({ modules, toggleLesson }) => (
 
   <aside>
     {modules.map((module) => (
@@ -15,7 +15,7 @@ const Sidebar = ({ modules, dispatch }) => (
           {module.lessons.map((lesson) => (
             <li key={lesson.id}>
               {lesson.title}
-              <button onClick={() => dispatch(CourseActions.toggleLesson(module, lesson))}>
+              <button onClick={() => toggleLesson(module, lesson)}>
                 Tornar ativo
               </button>  
             </li>
@@ -26,6 +26,14 @@ const Sidebar = ({ modules, dispatch }) => (
   </aside>
 );
 
+const mapStateToProps = state => ({
+  modules: state.course.modules
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleLesson: (module, lesson) => dispatch(CourseActions.toggleLesson(module, lesson))
+});
+
 // a funcção connect passa uma outa função com estado que eu quero 
 // e eu preciso retornar dessa função quais props eu quero daquele estado (nesse caso todas as props)
-export default connect(state => ({ modules: state.course.modules }))(Sidebar);
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
